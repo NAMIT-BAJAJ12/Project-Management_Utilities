d6b9e8a2d4d08372604dea6c3151f08d6cd8709a
NAMIT-BAJAJ12 <163017830+NAMIT-BAJAJ12@users.noreply.github.com>
2024-08-30 20:21:04 +0530

    Update Loader.c

--- Loader.c
+++ Loader.c
@@ -1,146 +1,131 @@
 #include "loader.h"
-#include "stdint.h"
-#define ERROR(msg) do { perror(msg); exit(1); } while(0)
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd;
-void *virt_mem; 
+int *data;
+Elf32_Phdr *virt_mem;
+// Macros for error handling, cleanup, and seeking
+#define ERROR(msg) do { printf("%s\n", msg);} while(0)
+#define ERROR_CLEANUP_EXIT(msg) do { printf("%s\n", msg); loader_cleanup(); exit(1); } while (0)
+#define SEEK(fd, offset, whence) do { if (lseek(fd, offset, whence) == -1) { ERROR_CLEANUP_EXIT("Failed in lseek"); } } while (0)
+#define CLEANUP_AND_EXIT() do { loader_cleanup(); exit(1); } while (0)
+
+// Function to release memory and other cleanups
 void loader_cleanup(){
     if (fd > 2){
         close(fd);
     }
     if (ehdr != NULL){
-        loader_cleanup(ehdr);
+        free(ehdr);
     }
     if (phdr != NULL){
-        loader_cleanup(phdr);
-    }
-    if (virt_mem != NULL){
-        munmap(virt_mem, phdr->p_memsz);  // This will unmap only if virt_mem was successfully mapped
+        free(phdr);
     }
 }
-void chk_off(off_t offset, const char *error_msg){
-    if (lseek(fd, offset, SEEK_SET) == (off_t) -1) {
-        ERROR(error_msg);
-    }
-}
-void verify_ELF(const char* filename) {
-    fd = open(filename, O_RDONLY);  // Open file for reading
-    if (fd < 0) {
-        ERROR("Error opening ELF file");
-    }
-    Elf32_Ehdr t_hdr;
-    chk_off(0, "Error seeking to the beginning of the file");
-    if (read(fd, &t_hdr, sizeof(t_hdr)) != sizeof(t_hdr)) {
-        close(fd);
-        ERROR("Error reading ELF header");
-    }
-    // Valid ELF magic number and other fields
-    if (t_hdr.e_ident[0] != 0x7f || t_hdr.e_ident[1] != 'E' || t_hdr.e_ident[2] != 'L' || t_hdr.e_ident[3] != 'F' ||
-        t_hdr.e_ident[EI_CLASS] != ELFCLASS32 || t_hdr.e_ident[EI_DATA] != ELFDATA2LSB ||
-        t_hdr.e_machine != EM_386 || t_hdr.e_ident[EI_VERSION] != EV_CURRENT ||
-        (t_hdr.e_type != ET_EXEC && t_hdr.e_type != ET_REL)) {
-        close(fd);
-        ERROR("Invalid or unsupported ELF file");
-    }
-    ERROR("ELF file is valid and supported\n");
-    close(fd);  // Closing the file descriptor after validation
-}
-void load_and_run_elf(char **argv){
-    fd = open(argv[1], O_RDONLY);
+// Function to verify if the file is a valid ELF file
+int verify_elf(const char *filename){
+    // Open the file
+    fd = open(filename, O_RDONLY);
     if (fd == -1) {
-        ERROR("Failed to open ELF file");
-        exit(1);
-    }
-    // 1. Load entire binary content into the memory from the ELF file.
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    if (file_size == -1){
-        ERROR("Failed in lseek, seeking to end of file");
-        loader_cleanup();
-        exit(1);
-    }
-    // Rewind to the beginning of the file
-    if (lseek(fd, 0, SEEK_SET) == -1){
-        ERROR("Failed in lseek, seeking to beginning of file   ...");
-        loader_cleanup();
-        exit(1);
+        ERROR_CLEANUP_EXIT("Error in opening ELF file ...\n");
     }
 
+    const char elf_chk[] = {0x7F, 'E', 'L', 'F'};
+    // Read the ELF header
+    Elf32_Ehdr *e_hdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
+    if (!e_hdr){
+    ERROR_CLEANUP_EXIT("Error allocating memory for ELF header");
+    }
+    // Read the ELF header
+    if (read(fd, &e_hdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)){
+        free(e_hdr);
+        ERROR_CLEANUP_EXIT("Error in reading ELF header\n");
+    }
+
+    // Validate the ELF magic number
+    if (memcmp(elf_chk, e_hdr.e_ident, 4) != 0) {
+        free(e_hdr);
+        ERROR_CLEANUP_EXIT("Invalid or unsupported file type\n");
+    }
+    close(fd);
+    return 1;
+}
+
+// Load and run the ELF executable file
+void load_and_run_elf(char **exe){
+    fd = open(exe[1], O_RDONLY);
+    if (fd == -1) {
+        ERROR_CLEANUP_EXIT("Failed in opening ELF file ...");
+    }
+    // 1. Load entire binary content into the memory from the ELF file.
+    long elf_s = lseek(fd, 0, SEEK_END);
+    if (elf_s == -1){
+        ERROR_CLEANUP_EXIT("Failed in lseek, seeking to end of file ...");
+    }
+    
+    // Rewind to the beginning of the file
+    SEEK(fd, 0, SEEK_SET);
+
     // Allocate memory to hold the ELF file
-    ehdr = (Elf32_Ehdr *)malloc(file_size);
-    if (!ehdr){
-        ERROR("Error allocating memory for ELF header");
-        loader_cleanup();
-        exit(1);
-    }
+    ehdr = (Elf32_Ehdr *)malloc(elf_s);
     phdr = (Elf32_Phdr *)malloc(sizeof(Elf32_Phdr));
-    if (!phdr){
-        ERROR("Error allocating memory for program header");
-        loader_cleanup();
-        exit(1);
-    }
+
     // Read the ELF file header into memory
     if (read(fd, ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)){
-        ERROR("Error reading ELF header");
-        loader_cleanup();
-        exit(1);
+        ERROR_CLEANUP_EXIT("Error in reading ELF content into memory ... ");
     }
-    // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-   //     type that contains the address of the entrypoint method in fib.c
-    chk_off(ehdr->e_phoff, "Error seeking to program header table");
-    for (int i = 0; i < ehdr->e_phnum; i++) {
-        if (read(fd, phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)){
-            ERROR("Error reading program header");
-            loader_cleanup();
-            exit(1);
+
+    // 2. Iterate through the PHDR table and find the section of PT_LOAD type
+    // that contains the address of the entry point method in fib.c
+    SEEK(fd, ehdr->e_phoff, SEEK_SET);
+    for (int i = 0; i < ehdr->e_phnum; i++){
+        ssize_t phdr_R = read(fd, phdr, sizeof(Elf32_Phdr));
+        if (phdr_R != sizeof(Elf32_Phdr)) {
+            ERROR_CLEANUP_EXIT("Error in reading phdr header ...");
         }
-    // 3. Allocate memory of the size "p_memsz" using mmap function 
-   //     and then copy the segment content
-        if (phdr->p_type == PT_LOAD && 
-            phdr->p_vaddr < ehdr->e_entry && 
-            ehdr->e_entry < phdr->p_memsz + phdr->p_vaddr){
-            // Map a region of memory to load the program segment
-            virt_mem = mmap(NULL, phdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-            chk_off(phdr->p_offset, "Error seeking to program segment");
-            if (virt_mem == MAP_FAILED) {
-                ERROR("Error mapping memory for program segment");
-                loader_cleanup();
-                exit(1);
+        // 3. Allocate memory of the size "p_memsz" using mmap function
+        // and then copy the segment content
+        if (phdr->p_type == PT_LOAD && phdr->p_vaddr < ehdr->e_entry && ehdr->e_entry < phdr->p_memsz + phdr->p_vaddr){
+        // Map a region of memory to load the program segment
+            virt_mem = mmap(NULL, phdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+            SEEK(fd, phdr->p_offset, SEEK_SET);
+            if (virt_mem == MAP_FAILED){
+                ERROR_CLEANUP_EXIT("Error in mapping a region of memory to load program segment ...");
             }
-            if (read(fd, virt_mem, phdr->p_filesz) != phdr->p_filesz){
-                ERROR("Error reading program data into memory");
+            // Read the program data and copy it to the mapped memory
+            ssize_t data_R = read(fd, virt_mem, phdr->p_memsz);
+            if (data_R != phdr->p_memsz){
+                ERROR("Error reading program data into virtual memory...");
                 loader_cleanup();
-                munmap(virt_mem, phdr->p_memsz);    
+                munmap(virt_mem, phdr->p_memsz);
                 exit(1);
             }
             break;
         }
     }
+    // End of for loop
 
-    // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
-    char *entry_point = (char *)virt_mem + (ehdr->e_entry - phdr->p_vaddr);
-
-    // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
-    int (_start)() = (int ()())entry_point;
-
+    // 4. Navigate to the entry point address into the segment loaded in the memory in above step
+    char *entry = (char *)virt_mem + (ehdr->e_entry - phdr->p_vaddr);
+    // 5. Typecast the address to that of a function pointer matching "_start" method in fib.c.
+    int (_start)() = (int ()())entry;
     // 6. Call the "_start" method and print the value returned from the "_start"
     int result = _start();
     printf("User _start return value = %d\n", result);
 }
-
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <ELF Executable>\n", argv[0]);
+        printf("Usage: %s <ELF Executable> \n", argv[0]);
         exit(1);
     }
-
-    // 1. carry out necessary checks on the input ELF file
-    verify_ELF(argv[1]);
-
-    // 2. passing it to the loader for carrying out the loading/execution
+    // Verify the ELF file
+    if (!verify_elf(argv[1])) {
+        exit(1);
+    }
+    // Passing it to the loader for carrying out the loading/execution
     load_and_run_elf(argv);
-
-    // 3. invoke the cleanup routine inside the loader  
+    // Invoke the cleanup routine inside the loader  
     loader_cleanup();
+    
     return 0;
 }
